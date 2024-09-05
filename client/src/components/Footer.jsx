a2b4c114bf8490ffb3d795b5ed148f1213bd7d9a
JuanJaramillo12004 <juan_edu.jaramillo@uao.edu.co>
2024-09-05 14:36:42 -0500

    Finalización del Footer y diseño del Login

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,50 +1,98 @@
 import { Image, Table } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import logoImage from "../assets/logo.png";
 import { colorTransparente } from "../utils/colorTemplate";
 
 const styleTable = {
     border: 'none',
-    backgroundColor: colorTransparente
+    backgroundColor: colorTransparente,
+    color: 'white',
+    fontSize: '14px'
 };
+
+const styleTable2 = {
+    textDecoration: 'none',
+    color: 'white'
+}
 
 const Footer = () => {
     return (
-        <div style={{backgroundColor: '#8306AD', padding: '2% 7%', width: '100%', height: 'auto', justifyContent: 'space-between', alignItems: 'center', borderRadius: '50px 50px 0 0', display: 'flex'}}>
-            <Table style={{borderCollapse: 'collapse', width: '100%', styleTable, color: 'white'}}>
-                <thead>
-                    <tr style={styleTable}>
-                    <th style={styleTable}>
-                        <Image src={logoImage} className="img-fluid" style={{ width: "80px", height: "80px"}} />
-                        Taskeria
-                    </th>
-                    <th style={styleTable}></th>
-                    <th style={styleTable}></th>
-                    <th style={styleTable}></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr style={styleTable}>
-                    <td style={styleTable}></td>
-                    <td style={styleTable}></td>
-                    <td style={styleTable}></td>
-                    <td style={styleTable}></td>
-                    </tr>
-                    <tr style={styleTable}>
-                    <td style={styleTable}></td>
-                    <td style={styleTable}></td>
-                    <td style={styleTable}></td>
-                    <td style={styleTable}></td>
-                    </tr>
-                    <tr style={styleTable}>
-                    <td style={styleTable}></td>
-                    <td style={styleTable}></td>
-                    <td style={styleTable}></td>
-                    <td style={styleTable}>hola</td>
-                    </tr>
-                </tbody>
-            </Table>
-        </div>
-    )
+        <>
+            <div style={{color: 'white', backgroundColor: '#8306AD', width: '100%', height: 'auto', justifyContent: 'space-between', borderRadius: '50px 50px 0 0', display: 'flex', flexDirection: 'column'}}>
+                <div style={{padding: '2% 7%'}}>
+                <Table style={{borderCollapse: 'collapse', width: '100%', styleTable}}>
+                        <thead>
+                            <tr style={styleTable}>
+                            <th style={styleTable}>
+                                <Image src={logoImage} className="img-fluid" style={{ width: "80px", height: "80px"}} />
+                                Taskeria
+                            </th>
+                            <th style={styleTable}>Categories</th>
+                            <th style={styleTable}>Help</th>
+                            <th style={styleTable}>Community</th>
+                            <th style={styleTable}>Company</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr style={styleTable}>
+                            <td style={styleTable}></td>
+                            <td style={styleTable}><Link to="/register" style={styleTable2}>Technology and Programming</Link></td>
+                            <td style={styleTable}><Link to="/login" style={styleTable2}>Help and Support</Link></td>
+                            <td style={styleTable}><Link to="/register" style={styleTable2}>Academy</Link></td>
+                            <td style={styleTable}><Link to="/register" style={styleTable2}>About Us</Link></td>
+                            </tr>
+                            <tr style={styleTable}>
+                            <td style={styleTable}></td>
+                            <td style={styleTable}><Link to="/register" style={styleTable2}>Graphic Design</Link></td>
+                            <td style={styleTable}><Link to="/register" style={styleTable2}>Trust and Security</Link></td>
+                            <td style={styleTable}><Link to="/register" style={styleTable2}>Blog</Link></td>
+                            <td style={styleTable}><Link to="/register" style={styleTable2}>Careers</Link></td>
+                            </tr>
+                            <tr style={styleTable}>
+                            <td style={styleTable}></td>
+                            <td style={styleTable}><Link to="/register" style={styleTable2}>Video and Animation</Link></td>
+                            <td style={styleTable}><Link to="/register" style={styleTable2}>Quality Guide</Link></td>
+                            <td style={styleTable}><Link to="/register" style={styleTable2}>Themes</Link></td>
+                            <td style={styleTable}><Link to="/register" style={styleTable2}>FAQs</Link></td>
+                            </tr>
+                            <tr style={styleTable}>
+                            <td style={styleTable}></td>
+                            <td style={styleTable}><Link to="/register" style={styleTable2}>Writing and Translation</Link></td>
+                            <td style={styleTable}><Link to="/register" style={styleTable2}>Taskeria Guides</Link></td>
+                            <td style={styleTable}><Link to="/register" style={styleTable2}>Hosting</Link></td>
+                            <td style={styleTable}><Link to="/register" style={styleTable2}>Teams</Link></td>
+                            </tr>
+                            <tr style={styleTable}>
+                            <td style={styleTable}></td>
+                            <td style={styleTable}><Link to="/register" style={styleTable2}>Digital Marketing</Link></td>
+                            <td style={styleTable}></td>
+                            <td style={styleTable}><Link to="/register" style={styleTable2}>Developers</Link></td>
+                            <td style={styleTable}><Link to="/register" style={styleTable2}>Contact Us</Link></td>
+                            </tr>
+                            <tr style={styleTable}>
+                            <td style={styleTable}></td>
+                            <td style={styleTable}><Link to="/register" style={styleTable2}>Music and Audio</Link></td>
+                            <td style={styleTable}></td>
+                            <td style={styleTable}><Link to="/register" style={styleTable2}>Support</Link></td>
+                            <td style={styleTable}></td>
+                            </tr>
+                        </tbody>
+                    </Table>
+                </div>
+                <div style={{display: 'flex', flexDirection: 'row', marginTop: '20px', margin: '0 1%', justifyContent: 'space-between'}}>
+                    <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'start'}}>
+                        <p style={{padding: '15px'}}>Privacy Policy</p>
+                        <p style={{padding: '15px'}}>Terms of Use</p>
+                        <p style={{padding: '15px'}}>Sales and Refunds</p>
+                        <p style={{padding: '15px'}}>Legal</p>
+                    </div>
+                    <div style={{justifyContent: 'end'}}>
+                        <p style={{padding: '15px'}}>© 2024 Taskeria. All rights reserved</p>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 }
 
 export default Footer;
--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -2,7 +2,8 @@ import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form"; // Used for form validation in React
 import { useNavigate, Link } from "react-router-dom";
 
-import { Form, Button } from "react-bootstrap";
+import { Form, Button, Container, Card, Image } from "react-bootstrap";
+import logoImage from "../assets/logo.png"; // Import the logo image
 
 import { useAuth } from "../context/AuthContext"; // Import the useAuth function from the AuthContext file
 import CustomToast from "../components/CustomToast"; // Import the CustomToast component
@@ -34,44 +35,97 @@ function LoginPage() {
   }, [signinErrors]);
 
   return (
-    <div className="container">
-      <h1>Login</h1>
+    <Container className="d-flex align-items-center justify-content-center min-vh-100 min-vw-100 bg-gradient-custom">
       <CustomToast
         show={showToast}
         message={signinErrors && signinErrors.message}
-        position={{ top: 0, right: 0 }}
+        position={{ top: "0", right: "0" }}
         duration={3000} // 3000 ms = 3 segundos
         onClose={() => setShowToast(false)}
       />
-      <Form onSubmit={onSubmit}>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control
-            type="email"
-            placeholder="Enter email"
-            {...register("email", { required: true })}
-          />
-          {errors.email && <p style={{ color: "red" }}>Email is required</p>}
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type="password"
-            placeholder="Password"
-            {...register("password", { required: true })}
-          />
-          {errors.password && (
-            <p style={{ color: "red" }}>Password is required</p>
-          )}
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Login
-        </Button>
-      </Form>
-      <p>
-        Don't you have an account? <Link to="/register">Sign up</Link>
-      </p>
-    </div>
+      <Card className="shadow-lg rounded-5 w-50 h-50 d-flex justify-content-center align-items-center">
+        <Card.Body className="mx-3 my-5 w-50 h-50">
+          <div className="d-flex justify-content-end">
+            <Button variant="link" href="/" className="p-0">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                className="bi bi-x"
+                viewBox="0 0 16 16"
+              >
+                <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708" />
+              </svg>
+            </Button>
+          </div>
+          <div className="text-center mb-4">
+            <Image
+              src={logoImage}
+              className="img-fluid"
+              style={{ width: "80px", height: "80px" }}
+            />
+            <h2 className="h4">Sign in</h2>
+            <p className="mt-3 text-center">
+              Don't you have an account? <Link to="/register" id="linkText">Sign up</Link>
+            </p>
+          </div>
+
+          <Container className="d-flex justify-content-center align-items-center">
+            <Button
+            className="w-100 border"
+            id="buttonGoogleLogin">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                class="bi bi-google"
+                viewBox="0 0 16 16"
+              >
+                <path d="M15.545 6.558a9.4 9.4 0 0 1 .139 1.626c0 2.434-.87 4.492-2.384 5.885h.002C11.978 15.292 10.158 16 8 16A8 8 0 1 1 8 0a7.7 7.7 0 0 1 5.352 2.082l-2.284 2.284A4.35 4.35 0 0 0 8 3.166c-2.087 0-3.86 1.408-4.492 3.304a4.8 4.8 0 0 0 0 3.063h.003c.635 1.893 2.405 3.301 4.492 3.301 1.078 0 2.004-.276 2.722-.764h-.003a3.7 3.7 0 0 0 1.599-2.431H8v-3.08z" />
+              </svg>
+              <strong> Sign in with Google</strong>
+            </Button>
+          </Container>
+
+          <div class="divider">OR</div>
+
+          <Form onSubmit={handleSubmit(onSubmit)}>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="Enter email"
+                {...register("email", { required: true })}
+              />
+              {errors.email && <p className="text-danger">Email is required</p>}
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                {...register("password", { required: true })}
+              />
+              {errors.password && (
+                <p className="text-danger">Password is required</p>
+              )}
+            </Form.Group>
+            <div className="d-flex justify-content-end m-2">
+              <Link to="/register" id="linkText">Forget your password</Link>
+            </div>
+            <Button
+              variant="primary"
+              type="submit"
+              className="w-100"
+              id="buttonLogin"
+            >
+              Login
+            </Button>
+          </Form>
+        </Card.Body>
+      </Card>
+    </Container>
   );
 }
 
